22fc2ca103137cbfd55dd30cdd5659ee3e0765e3
voidnowhere <alivnw@outlook.com>
2023-04-11 19:01:39 +0000

    Customer or reviewer can login & register & logou and manage his account

---
+++ users/models.py
@@ -0,0 +1,46 @@
+from uuid import uuid4
+
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.db import models
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    CUSTOMER = 'C'
+    REVIEWER = 'R'
+    TYPES = [
+        (CUSTOMER, 'Player'),
+        (REVIEWER, 'Owner'),
+    ]
+
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    email = models.EmailField(unique=True)
+    type = models.CharField(max_length=1, choices=TYPES)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+
+    objects = UserManager()
